f9da4f7a57dd092a13011201b96d1e822db6e5ad
Thijs Boom <twboom@hotmail.nl>
2021-06-06 18:13:10 +0200

    Cleanup code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
 const players = [];
 
 function update() {
-    const playerlist = document.getElementById('players')
-    playerlist.innerHTML = ''
+    const playerlist = document.getElementById('players');
+    playerlist.innerHTML = '';
     for (player in players) {
-        const el = document.createElement('li')
+        const el = document.createElement('li');
         el.innerText = players[player].uuid;
-        playerlist.appendChild(el)
+        playerlist.appendChild(el);
     };
 };
 
 function search() {
-    document.getElementById('results').innerHTML = ''
+    document.getElementById('results').innerHTML = '';
     let query = document.getElementById('query').value;
     const cat = document.getElementById('cat').value;
     if (query === '' || players.length === 0) { return }
     query = 'minecraft:' + query;
     for (let i = 0; i < players.length; i++) {
-        const player = players[i]
-        const ct = player.stats[cat]
-        if (ct === undefined) { continue }
-        let result = ct[query]
+        const player = players[i];
+        const ct = player.stats[cat];
+        if (ct === undefined) { continue };
+        let result = ct[query];
         if (result === undefined) { result = 0 };
-        console.log(`${player.uuid}: ${result}`);
         const tr = document.createElement('tr');
         const count = document.createElement('td');
         count.innerText = result;
         const uuid = document.createElement('td');
         uuid.innerText = player.uuid;
-        tr.appendChild(count)
-        tr.appendChild(uuid)
-        document.getElementById('results').appendChild(tr)
+        tr.appendChild(count);
+        tr.appendChild(uuid);
+        document.getElementById('results').appendChild(tr);
     };
 };
 
@@ -46,22 +45,21 @@ function process(file) {
     if (file === undefined) { return }
     const uuid = file.name.replace('.json', '')
     file.text().then(r => JSON.parse(r)).then(json => {
-        const pl = new Player(json, uuid)
-        if (players.includes(pl)) { return }
-        players.push(pl)
-        update()
+        const pl = new Player(json, uuid);
+        if (players.includes(pl)) { return };
+        players.push(pl);
+        update();
     });
 };
 
 function init() {
     document.getElementById('upload').addEventListener('change', _ => {
-        const files = document.getElementById('upload').files
-        //process(document.getElementById('upload').files[0])
+        const files = document.getElementById('upload').files;
         for (let i = 0; i < files.length; i++) {
-            process(files[i])
-        }
+            process(files[i]);
+        };
     });
-    document.getElementById('search').addEventListener('click', search)
+    document.getElementById('search').addEventListener('click', search);
 };
 
 window.onload = init;
