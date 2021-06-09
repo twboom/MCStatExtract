964d9056ad72bbee08ad1fac3c42697a9e722dbd
Thijs Boom <twboom@hotmail.nl>
2021-06-09 14:36:10 +0200

    Made splitting work

--- js/log.js
+++ js/log.js
@@ -1,17 +1,32 @@
 const lines = [];
 
 function process(file) {
-
+    if (file === undefined) { return };
+    file.text().then(data => {
+        for (index in data.split(/\r?\n/)) {
+            let line = data.split(/\r?\n/)[index];
+            if (line === '') { continue };
+            line = new Line(line)
+            console.log(line)
+            lines.push(line)
+        }
+    })
 }
 
 
 class Line {
 
-    constructor(time, initiator, content) {
+    constructor(line) {
 
-        this.time = time;
-        this.initiator = initiator;
-        this.content = content;
+        this.line = line;
+        this.time = line.split(' ')[0].replace('[', '').replace(']', '')
+        this.initiator = line.split('[')[2].split(']')[0].split('/')[0]
+        this.type = line.split('[')[2].split(']')[0].split('/')[1]
+        this.content = line.split(']')[2].substring(2)
+        for (let i = 2; i < line.split(']').length; i++) {
+            this.content += ']'
+            this.content += line.split(']')[i]
+        }
 
     }
 
@@ -28,9 +43,7 @@ function init() {
     // Upload files
     document.getElementById('upload').addEventListener('change', _ => {
         const files = document.getElementById('upload').files;
-        for (let i = 0; i < files.length; i++) {
-            process(files[i]);
-        };
+        process(files[0])
     });
 
 }
