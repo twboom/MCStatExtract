f3367b3074d6160eba9888bc73d75a0511093359
Thijs Boom <twboom@hotmail.nl>
2021-06-09 14:40:14 +0200

    Fixed some issues

--- js/log.js
+++ js/log.js
@@ -16,21 +16,22 @@ function process(file) {
 
 class Line {
 
-    constructor(line) {
+    constructor(line, index) {
 
+        this.index = index;
         this.line = line;
-        this.time = line.split(' ')[0].replace('[', '').replace(']', '')
-        this.initiator = line.split('[')[2].split(']')[0].split('/')[0]
-        this.type = line.split('[')[2].split(']')[0].split('/')[1]
-        this.content = line.split(']')[2].substring(2)
-        for (let i = 2; i < line.split(']').length; i++) {
-            this.content += ']'
-            this.content += line.split(']')[i]
-        }
+        this.time = line.split(' ')[0].replace('[', '').replace(']', '');
+        this.initiator = line.split('[')[2].split(']')[0].split('/')[0];
+        this.type = line.split('[')[2].split(']')[0].split('/')[1];
+        this.content = line.split(']')[2].substring(2);
+        for (let i = 3; i < line.split(']').length; i++) {
+            this.content += ']';
+            this.content += line.split(']')[i];
+        };
 
-    }
+    };
 
-}
+};
 
 
 function init() {
