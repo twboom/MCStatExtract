27b91ee1aa5331891bdf1e564e6bc002332fb7ae
Thijs Boom <twboom@hotmail.nl>
2022-07-28 17:09:47 +0200

    Deleted files

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    document.querySelectorAll('.tool-button').forEach(el => {
-        el.addEventListener('click', _ => {
-            location.href = el.dataset.link
-        })
-    })
-}
-
-window.onload = init
